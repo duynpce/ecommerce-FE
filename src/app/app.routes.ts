9bc6c838e8ebf4634dd7ece1b22e5b488cc37526
duynpce <a23.thvl03@gmail.com>
2026-06-27 18:32:13 +0700

    feat: create angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'register',
+		loadComponent: () =>
+			import('../feat/auth/register/register.component').then(
+				(m) => m.RegisterComponent
+			),
+	},
+	{
+		path: 'callback/:authServer',
+		loadComponent: () =>
+			import('../feat/auth/callback/callback.component').then(
+				(m) => m.CallbackComponent
+			),
+	},
+	{
+		path: 'logout',
+		loadComponent: () =>
+			import('../feat/auth/logout/logout.component').then((m) => m.LogoutComponent),
+	},
+	{
+		path: '',
+		loadComponent: () =>
+			import('../feat/home/home.component').then((m) => m.HomeComponent),
+	},
+	{
+		path: '**',	
+		redirectTo: '',
+	},
+];
